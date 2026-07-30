0e755e7e0aa56ac91f1ea8ddded511d059d2b52e
Caratse <976915378@qq.com>
2026-07-30 14:45:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "daily-budget-cloud-v50-analysis-charts-20260730";
+const CACHE_NAME = "daily-budget-cloud-v51-income-pin-analysis-20260730";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./firebase-config.js?v=50",
-  "./firebase-cloud-v50.js?v=50",
+  "./firebase-config.js?v=51",
+  "./firebase-cloud-v51.js?v=51",
   "./icon-192.png",
   "./icon-512.png"
 ];
